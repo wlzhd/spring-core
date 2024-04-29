553cc6e997855cf3fbd34601e17347ec3e56fcef
PC09\USER <jtr6120@naver.com>
2024-04-29 16:40:21 +0900

    no message

---
+++ code/src/test/java/hellow/code/CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package hellow.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
